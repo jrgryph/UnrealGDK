87c6fa3d4ad3256036e62d233468ab06ae43cfea
Vlad Barvinko <vladbarvinko@improbable.io>
2019-11-13 17:20:18 +0000

    move google credentials override

--- ci/upload-test-metrics.sh
+++ ci/upload-test-metrics.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 set -euo pipefail
 
+# Fetch the test summary artifacts uploaded earlier
+mkdir "ci/test_summaries"
+buildkite-agent artifact download "*test_summary*.json" "ci/test_summaries"
+
 export GOOGLE_APPLICATION_CREDENTIALS="$(mktemp)"
 
 # Fetch Google credentials so that our python script can upload the metrics to the GCS bucket.
 imp-ci secrets read --environment=production --buildkite-org=improbable \
     --secret-type=gce-key-pair --secret-name=prod-research-gcp \
     --write-to=$GOOGLE_APPLICATION_CREDENTIALS
-
-# Fetch the test summary artifacts uploaded earlier
-mkdir "ci/test_summaries"
-buildkite-agent artifact download "*test_summary*.json" "ci/test_summaries"
 
 # Upload test summaries to GCS
 python "ci/upload-test-metrics.py"
