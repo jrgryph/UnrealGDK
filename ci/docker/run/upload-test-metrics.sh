f72701151a60f432cedea028b65f8edcf0492cdb
Vlad Barvinko <vladbarvinko@improbable.io>
2019-11-14 18:01:58 +0000

    add debug printing

--- ci/docker/run/upload-test-metrics.sh
+++ ci/docker/run/upload-test-metrics.sh
@@ -7,6 +7,8 @@ buildkite-agent artifact download "*test_summary*.json" "./test_summaries"
 
 export GOOGLE_APPLICATION_CREDENTIALS="$(mktemp)" 
 
+ls /usr/local/bin/imp-ci
+
 # Fetch Google credentials so that our python script can upload the metrics to the GCS bucket.
 /usr/local/bin/imp-ci secrets read --environment=production --buildkite-org=improbable \
     --secret-type=gce-key-pair --secret-name=prod-research-gcp \
